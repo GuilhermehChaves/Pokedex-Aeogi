6895be39ee28bf69b26103f8b8ed850a9d3a0171
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2019-11-30 16:48:30 -0300

    Estrutura inicial do projeto

---
+++ Database/Database.py
@@ -0,0 +1,12 @@
+import pymysql
+
+class Database:
+
+  def __init__(self,host,user,password,database):
+    self.host = host
+    self.user = user
+    self.password = password
+    self.database = database
+    
+
+
