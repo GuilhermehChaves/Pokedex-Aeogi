d27acc020b4bdd3c1f2de16c63d7b05e7bfa9646
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2019-12-06 11:35:48 -0300

    criado função que insere pokemons e tipos

--- fill_database.py
+++ fill_database.py
@@ -1,55 +1,63 @@
-import db
+from modules import db
 import csv
 
-pokedex = csv.DictReader(open("./Info/pokemon.csv", encoding = 'utf8'))
+pokedex = csv.DictReader(open("./src/Info/pokemon.csv", encoding = 'utf8'))
 cursor = db.cursor
 
 
-counter = 0
-
-for row in pokedex:
-  sql = "INSERT INTO pokemon VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-
-  values = (int(row['pokedex_number']),
-            row['name'],
-            int(row['attack']),
-            row['classfication'],
-            int(row['defense']),
-            row['height_m'],
-            int(row['hp']),
-            int(row['speed']),
-            row['type1'],
-            row['type2'],
-            row['weight_kg'],
-            int(row['generation']),
-            int(row['is_legendary'])
-           )
-
-  cursor.execute(sql,values)
-  counter += 1
-  print(counter, "Successed insert...")
-  db.CONNECTION.commit()
-
-db.CONNECTION.close()
 
 
+def savePokemons():
+  counter = 0
+  for row in pokedex:
+    sql = "INSERT INTO pokemon VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+    values = (int(row['pokedex_number']),
+              row['name'],
+              int(row['attack']),
+              row['classfication'],
+              int(row['defense']),   
+              row['height_m'],
+              int(row['hp']),
+              int(row['speed']),
+              row['weight_kg'],
+              int(row['is_legendary']),
+              int(row['generation'])
+            )
+            
+    cursor.execute(sql,values)
+    counter += 1
+    print(counter, "Successed insert...")
+    db.CONNECTION.commit()
+
+def getDiferentTypes():
+  types = []
+
+  for row in pokedex:  
+    if row['type1'] not in types:
+      types.append(row['type1'])
+
+    if row['type2'] not in types:
+      types.append(row['type2'])
+
+  return types
 
 
+def saveTypes():
+  types = getDiferentTypes()
+  print(len(types))
+
+  for i in range(len(types)):
+    sql = "INSERT INTO type (name) VALUE(%s)"
+    value = (types[i])
+    print(types[i])
+
+    cursor.execute(sql, value)
+    print('Successed insert...')
+    db.CONNECTION.commit()
 
 
-
-
-
-
-
-
-
-
-  # # index = ['pokedex_number', 'name', 'attack', 'classfication', 'defense',        
-  # #         'height_m', 'hp', 'speed', 'type1', 'type2', 'weight_kg', 'generation',
-  # #         'is_legendary', 'abilities']
-
-  
-
-  # for i in range(index):    
-  #   print(pokedex['pokedex_number'])
+print('Inserting Pokemons...')
+savePokemons()
+print('Inserting Types...')
+saveTypes()
