a2ebefd6ed58d8a818c99e9099b145b1248c09df
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2019-12-06 22:16:29 -0300

    Modificação no fill abilities

--- modules/fill/fill_abilities.py
+++ modules/fill/fill_abilities.py
@@ -1,20 +1,35 @@
 from modules import db
 import csv
+import numpy as np
 
 def insert():
     pokedex = csv.DictReader(open("./src/Info/pokemon.csv", encoding = 'utf8'))
     cursor = db.cursor
 
     abilities = []
-    remove = "[],"
+    remove = "[]"
 
     for row in pokedex:
 
       ability = row['abilities']
 
       for i in range(len(remove)):
-        print(ability)
         ability = ability.replace(remove[i], "")
+
+        if i == 1:
+          abilities.append(ability.split(", "))
+  
+
+    abilities = np.asarray(abilities)
+    print(len(abilities))
+    print(len(abilities[10]))
+    abilities.reshape(-1, 3)
+
+
+
+    # for i in range(len(abilities)):
+    #   print(abilities[i])
+    
 
        
 
