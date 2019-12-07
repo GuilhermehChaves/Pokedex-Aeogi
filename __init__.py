53c389f3e1cb7ad024f2390e2b4adceb1f80b8ad
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2019-12-07 10:25:30 -0300

    Efetuando busca por id ou nome

--- __init__.py
+++ __init__.py
@@ -14,10 +14,22 @@ def index():
                                             'https://assets.pokemon.com/assets/cms2/img/pokedex/full/0',
                                             'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'],
                                       pokemons = pokemons,
-                                      types = types
-                        )
+                                      types = types)
 
 
+
+
+@app.route('/pokemon/<string:search>', methods = ["GET"])
+def pokemon(search):
+
+  pokemon = Pokemon.selectOne(search)
+  title = pokemon[0]['name']
+
+  return render_template('pokemon.html', title=title,
+                                         pokemon=pokemon,
+                                         img=['https://assets.pokemon.com/assets/cms2/img/pokedex/full/00',
+                                              'https://assets.pokemon.com/assets/cms2/img/pokedex/full/0',
+                                              'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'] )
 app.run()
 
 
--- src/Controller/Pokemon.py
+++ src/Controller/Pokemon.py
@@ -9,4 +9,12 @@ def selectAll():
   
   return pokemons
 
+def selectOne(search):
+  cursor = db.cursor
+  sql = "SELECT * FROM pokemon WHERE pokedex_number = %s OR name = %s"
+  values = (search, search)
+  cursor.execute(sql, values)
+  pokemons = cursor.fetchall()
+  db.CONNECTION.commit()
   
+  return pokemons
